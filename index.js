8f96a65db4a54f2e2bae4f2edd1a19228f33c34a
Jordan Rantucci <jordanrantucci@gmail.com>
2021-02-07 17:02:53 -0500

    adding manager question prompts

--- index.js
+++ index.js
@@ -10,3 +10,34 @@ const Manager = require('./lib/Manager.js')
 const writeFile = util.promisify(fs.writeFile)
 
 teamArray = []
+
+// writefile using inquirer prompts to user about each employee
+const promptManager = () =>
+    inquirer.prompt([
+        {
+            type: 'input',
+            message: 'What is the name of the Team Manager?',
+            name: 'managerName'
+        },
+        {
+            type: 'input',
+            message: 'What is the ID Number?',
+            name: 'ID'
+        },
+        {
+            type: 'input',
+            message: "What is the Manager's email?",
+            name: 'managerEmail'
+        },
+        {
+            type: 'input',
+            message: "What is the Manager's Office Phone Number?",
+            name: 'phoneNumber'
+        },
+        {
+            type: 'list',
+            message: 'Which team member would you like to add?',
+            choices: ['Engineer', 'Intern', 'My team is complete' ],
+            name: 'teamMember'
+        }
+    ])
